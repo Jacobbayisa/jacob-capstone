8c512d4837aacbb5b3e8f6ef8ad8a6e24c448449
Jacob Bayisa <bayisagobu@gmail.com>
2021-05-30 01:30:44 -0400

    secure file removed from github

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -3,8 +3,8 @@ module.exports = {
   connection: {
     host: "127.0.0.1",
     user: "jacob",
-    password: "process.env.PASSWORD",
-    database: "toronto_rental",
+    password: process.env.PASSWORD,
+    database: process.env.DBNAME,
     charset: "utf8",
   },
 };
